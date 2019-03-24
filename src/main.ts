d7f682fb32feaaf75b65a084a4a2f6c9423421db
Piotr Bender <48630647+SIN3d73@users.noreply.github.com>
2019-03-24 17:43:01 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {ValidationPipe} from '@nestjs/common';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors();
+
     const options = new DocumentBuilder()
         .setTitle('GenericDemo API')
         .setDescription('Full API below')
